c836f14968673f90ba0e83bdf4434fced137a31c
AndreaPerquoti <ap92@hotmail.it>
2022-11-14 22:56:40 +0100

    first commit

---
+++ src/main/java/demo/demoarchetype/DemoArchetypeApplication.java
@@ -0,0 +1,13 @@
+package demo.demoarchetype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoArchetypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoArchetypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/demoarchetype/DemoArchetypeApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.demoarchetype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoArchetypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
